52f1c7e3551d4c14515a8be073ae3025682beba2
Dheeraj Suthar <dheersuthar@gmail.com>
2018-10-16 17:47:41 +0530

    Exercise 5

---
+++ 05-list-overlap.py
@@ -0,0 +1,9 @@
+import random
+randA = random.sample(range(1,40),random.randint(10,20))
+randB = random.sample(range(1,40),random.randint(10,20))
+print('randA = ',randA)
+print('randB = ',randB)
+res = [i for i in randA if i in randB]
+print(set(res))
+# or
+print(set(randA) & set(randB))
